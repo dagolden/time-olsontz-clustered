30d0bf3731981db74d6b2d727a1fe604ff8aa6f7
David Golden <dagolden@cpan.org>
2013-01-12 21:47:49 -0500

    primary_zones() implemented with basic test

--- lib/Time/OlsonTZ/Clustered.pm
+++ lib/Time/OlsonTZ/Clustered.pm
@@ -3,11 +3,55 @@ use strict;
 use warnings;
 
 package Time::OlsonTZ::Clustered;
-# ABSTRACT: Clusters Olson time zones based on similar offset and DST changes
+# ABSTRACT: Olson time zone clusters based on similar offset and DST changes
 # VERSION
 
-# Dependencies
+use Sub::Exporter -setup => { exports => [qw/primary_zones/] };
 
+use File::ShareDir::Tarball qw/dist_file/;
+use Path::Class;
+use Sereal::Decoder qw/decode_sereal/;
+
+{
+    my $clusters;
+    my $reverse;
+
+    sub _clusters {
+        return $clusters if defined $clusters;
+        my $file = dist_file( 'Time-OlsonTZ-Clustered', 'cluster.srl' )
+          or die "Can't find cluster.srl in distribution share data";
+        $clusters = decode_sereal( scalar file($file)->slurp );
+    }
+
+    sub _reverse_map {
+        return $reverse if defined $reverse;
+        my $file = dist_file( 'Time-OlsonTZ-Clustered', 'reverse.srl' )
+          or die "Can't find reverse.srl in distribution share data";
+        $reverse = decode_sereal( scalar file($file)->slurp );
+    }
+}
+
+sub primary_zones {
+    my ($code) = @_;
+
+    my $country      = _clusters()->{$code};
+    my $clusters     = $country->{clusters};
+    my $order        = $country->{cluster_order};
+    my $country_name = $country->{olson_name};
+
+    my @zones;
+    for my $c (@$order) {
+        my $description = $clusters->{$c}{description};
+        my $first       = $clusters->{$c}{zones}[0];
+        my %primary     = (
+            description => $description || $country_name,
+            offset => $first->{offset},
+            timezone_name => $first->{timezone_name},
+        );
+        push @zones, \%primary;
+    }
+    return \@zones;
+}
 
 1;
 
@@ -32,4 +76,4 @@ Maybe other modules do related things.
 
 =cut
 
-# vim: ts=2 sts=2 sw=2 et:
+# vim: ts=4 sts=4 sw=4 et:
